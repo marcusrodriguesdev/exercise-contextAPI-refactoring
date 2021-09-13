ec3b323065d504a46a36c16e35f3b490234debd3
marcus <marcus.rodrigues10@hotmail.com>
2021-09-13 20:21:47 -0300

    finish exercise one

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import Provider from './context/Provider';
 import App from './App';
 
-import store from './redux';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={ store }>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
+
